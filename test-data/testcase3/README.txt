41635bc7fa7e4bbe222760126294e2f20d329dc3
Yusong Yue <samyue1975@gmail.com>
2014-05-18 18:43:06 +1000

    Added 1 more test case

---
+++ test-data/testcase3/README.txt
@@ -0,0 +1,2 @@
+This is a more complicated test case. There are mutliple ingredients with same name but different expiration time.
+
--- test/RecipeFinder_test.js
+++ test/RecipeFinder_test.js
@@ -55,4 +55,17 @@ exports['awesome'] = {
         test.done();
     });
   },
+  'testcase3': function(test) {
+    test.expect(3);
+    
+    // tests here
+    RecipeFinder.run('test-data/testcase3/recipes.json', 
+      'test-data/testcase3/fridge.csv', 
+      function(err, result) {
+        test.equal(err, null, 'Should not have error.');
+        test.ok(result, 'should have result');
+        test.equal(result.recipe.name, "grilled cheese on toast");
+        test.done();
+    });
+  },
 };
